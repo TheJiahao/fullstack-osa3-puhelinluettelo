1b343824379949d12ea5577c4595248dc52dd2f6
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-02 11:28:28 +0200

    Lisää t3.2

--- index.ts
+++ index.ts
@@ -30,6 +30,18 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const currentTime = new Date();
+  console.log(currentTime);
+
+  response.send(
+    [
+      `<p>Phonebook has info for ${persons.length} people</p>`,
+      `<p>${currentTime.toString()}</p>`,
+    ].join("\n")
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
