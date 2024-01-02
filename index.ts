508afcb0c181d1131d9c13506c94a31393d5e469
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-02 10:55:31 +0200

    Poista turha requiren import

--- index.ts
+++ index.ts
@@ -1,7 +1,4 @@
 import { Application } from "express";
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
 
 const express = require("express");
 const app: Application = express();
