6af84d7f543ec47671dcb5129fa9f67058217e1b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-03 16:56:20 +0200

    Refaktoroi turhat koodit pois

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 import { Application } from "express";
-import Person from "./interfaces/Person";
 import initialize_routes from "./routes";
 
 const express = require("express");
@@ -26,54 +25,7 @@ app.use(
 app.use(cors());
 app.use(express.static("dist"));
 
-let persons: Person[] = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
 initialize_routes(app);
-
-app.post("/api/persons", (request, response) => {
-  const person = { ...request.body };
-  console.log("Person in request", person);
-
-  if (!("number" in person && "name" in person)) {
-    response.status(400);
-    response.send({ error: "Name or number missing." }).end();
-
-    return;
-  }
-
-  if (persons.map((person) => person.name).includes(person.name)) {
-    response.status(409);
-    response.send({ error: "Name already added." }).end();
-  }
-
-  person.id = Math.floor(Math.random() * 10000000);
-
-  persons = persons.concat(person);
-  console.log("Added person", person);
-
-  response.json(person).end();
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
